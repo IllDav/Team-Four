6a04fc311ee95e08b71cfa1416d632749cde7086
mb998 <muzsek.balazs@gmail.com>
2020-06-04 17:20:19 +0200

    evaluation magyarázat

--- apps/Evaluation.js
+++ apps/Evaluation.js
@@ -8,6 +8,7 @@ export class Evaluation extends Application {
         this.initialize();
     }
 
+	//Amint rányomunk az Értékelés gombra, ez a függvény fut le
     initialize() {
 		document.getElementById('eval').style.display = 'none';
 		
@@ -15,7 +16,8 @@ export class Evaluation extends Application {
         this.canvas.appendChild(h11);
         h11.textContent = 'Értékelés';
 		
-		let fEszk = document.getElementById('forgEszk').value;
+		//Itt létrehozzuk a változókat, és egyenlővé tesszük a megfelelő input mezőbe írt értékekkel
+		let fEszk = document.getElementById('forgEszk').value;   //változó = input a html-ből
 		let ePapir = document.getElementById('ertPap').value;
 		let pEszk = document.getElementById('penzEszk').value;
 		let sToke = document.getElementById('sajToke').value;
@@ -24,18 +26,22 @@ export class Evaluation extends Application {
 		let hlk = document.getElementById('hosszLejKot').value;
 		let rlk = document.getElementById('rovLejKot').value;
 		
-		const l1 = document.createElement('label');
-        this.canvas.appendChild(l1);
-        l1.textContent = 'Likviditás I.:';
+		//Ez már a kiíratás, mindegyik mutatószámhoz kell majd egy label, és egy p, amibe kerül a százalékos érték
+		const l1 = document.createElement('label'); //megfelelő tag létrehozása
+        this.canvas.appendChild(l1);  //Hozzáfűzás a HTML-hez
+        l1.textContent = 'Likviditás I.:';  //Tartalmának megadása
 		
+		//Itt az értékek kiírása történik, hasonlóan mint felette
 		const likv1 = document.createElement('p');
         this.canvas.appendChild(likv1);
         likv1.textContent = (((fEszk / rlk).toFixed(4))*100) + '%';
 		
-		const save = document.createElement('button');
-        this.canvas.appendChild(save);
-        save.textContent = 'Mentés PDF-ként';
-        save.id = 'savePDF';
+		
+		//Egyelőre nem fontos
+		// const save = document.createElement('button');
+        // this.canvas.appendChild(save);
+        // save.textContent = 'Mentés PDF-ként';
+        // save.id = 'savePDF';
 		
 	}
 
