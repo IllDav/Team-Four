19bd94406ff097e50d920afbafba1e5dc5e57126
mb998 <muzsek.balazs@gmail.com>
2020-06-04 09:45:41 +0200

    valtozok kinyerese 1

--- apps/Evaluation.js
+++ apps/Evaluation.js
@@ -9,9 +9,28 @@ export class Evaluation extends Application {
     }
 
     initialize() {
+		document.getElementById('eval').style.display = 'none';
+		
         const h11 = document.createElement('h1');
         this.canvas.appendChild(h11);
         h11.textContent = 'Értékelés';
+		
+		let fEszk = document.getElementById('forgEszk').value;
+		let ePapir = document.getElementById('ertPap').value;
+		let pEszk = document.getElementById('penzEszk').value;
+		let sToke = document.getElementById('sajToke').value;
+		let aEred = document.getElementById('adEredm').value;
+		let kot = document.getElementById('kotel').value;
+		let hlk = document.getElementById('hosszLejKot').value;
+		let rlk = document.getElementById('rovLejKot').value;
+		
+		const l1 = document.createElement('label');
+        this.canvas.appendChild(l1);
+        l1.textContent = 'Likviditás I.:';
+		
+		const likv1 = document.createElement('p');
+        this.canvas.appendChild(likv1);
+        likv1.textContent = (((fEszk / rlk).toFixed(4))*100) + '%';
 		
 		const save = document.createElement('button');
         this.canvas.appendChild(save);
