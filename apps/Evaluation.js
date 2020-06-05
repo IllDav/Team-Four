cb30ddfec10351b1ed0ebee73da1d93407e6dd7b
mb998 <muzsek.balazs@gmail.com>
2020-06-05 13:23:01 +0200

    első if

--- apps/Evaluation.js
+++ apps/Evaluation.js
@@ -25,6 +25,17 @@ export class Evaluation extends Application {
 		let kot = document.getElementById('kotel').value;
 		let hlk = document.getElementById('hosszLejKot').value;
 		let rlk = document.getElementById('rovLejKot').value;
+		let osszE = document.getElementById('osszEszk').value;
+		let osszF = document.getElementById('osszForr').value;
+		
+        let ert_net_arb = document.getElementById("ert_net_arb").value; 
+        let anyag_kolt = document.getElementById("anyag_kolt").value; 
+        let igenybe_szolg = document.getElementById("igenybe_szolg").value; 
+        let egyeb_szolg = document.getElementById("egyeb_szolg").value; 
+        let anyag_raford = document.getElementById("anyag_raford").value; 
+        let szemely_jel_raford = document.getElementById("szemely_jel_raford").value; 
+        let uzem_eredmeny = document.getElementById("uzem_eredmeny").value; 
+        let ossz_koltseg = document.getElementById("ossz_koltseg").value; 
 		
 		//Ez már a kiíratás, mindegyik mutatószámhoz kell majd egy label, és egy p, amibe kerül a százalékos érték
 		const l1 = document.createElement('label'); //megfelelő tag létrehozása
@@ -34,55 +45,76 @@ export class Evaluation extends Application {
 		//Itt az értékek kiírása történik, hasonlóan mint felette
 		const likv1 = document.createElement('p');
         this.canvas.appendChild(likv1);
-        likv1.textContent = (((fEszk / rlk).toFixed(4))*100) + '%';
-        
-        
-        
-        
-        let ert_net_arb = document.getElementById("ert_net_arb").value; 
-        let anyag_kolt = document.getElementById("anyag_kolt").value; 
-        let igenybe_szolg = document.getElementById("igenybe_szolg").value; 
-        let egyeb_szolg = document.getElementById("egyeb_szolg").value; 
-        let anyag_raford = document.getElementById("anyag_raford").value; 
-        let szemely_jel_raford = document.getElementById("szemely_jel_raford").value; 
-        let uzem_eredmeny = document.getElementById("uzem_eredmeny").value; 
+		var lik1 = fEszk / rlk;
+        likv1.textContent = (lik1 * 100).toFixed(2) + '%';
+		
+		
+		
+		
+		//--------------------------------------ÚJ------------------------------------
+		
+		
+		
+		
+		const likv1_text = document.createElement('p');
+        this.canvas.appendChild(likv1_text);
+		var l1_text = "";
+		
+		if(lik1 < 0.4)
+		{
+			l1_text = "bla bla Likviditás rossz"; //ezek helyére egy szebben fogalmazott verzió kerül majd
+		}
+		else if(lik1 >= 0.4 && lik1 <=0.7)
+		{
+			l1_text = "bla bla Likviditás elfogadható";
+		}
+		else
+		{
+			l1_text = "bla bla Likviditás jó";
+		}
+        likv1_text.textContent = l1_text;
 		
         
+		
+		//------------------------------------------ÚJ------------------------------------
+		
+		
+		
         const l2 = document.createElement('label'); 
         this.canvas.appendChild(l2);
         l2.textContent = 'Likviditás III.:';
         
-        const likv2 = document.createElement('p');
-        this.canvas.appendChild(likv2);
-        likv2.textContent = (+pEszk + +ePapir)/rlk + '%';  
-        //---------------------------------------    
-        
-    
-        const l3 = document.createElement('label'); 
-        this.canvas.appendChild(l3);
-        l3.textContent = 'Saját tőke aránya (tulajdonosi arány) vagy tőkeellátottság (tőkeerősség).:';
-        
         const likv3 = document.createElement('p');
         this.canvas.appendChild(likv3);
-        likv3.textContent = sToke/(+sToke + +aEred+ +kot + +hlk + +rlk) + '%';  //nem biztos hogy így kell számolni
+        likv3.textContent = (((pEszk + ePapir) / rlk)*100).toFixed(2) + '%';  
+        //---------------------------------------    
+        
+        const l3 = document.createElement('label'); 
+        this.canvas.appendChild(l3);
+        l3.textContent = 'Saját tőke aránya:';
+        
+        const sta = document.createElement('p');
+        this.canvas.appendChild(sta);
+        sta.textContent = ((sToke/osszF)*100).toFixed(2) + '%';
         //---------------------------------------     
         
         const l4 = document.createElement('label'); 
         this.canvas.appendChild(l4);
-        l4.textContent = 'Idegen tőke saját tőke aránya (kötelzettségek részaránya):';
+        l4.textContent = 'Idegen tőke aránya:';
         
-        const likv4 = document.createElement('p');
-        this.canvas.appendChild(likv4);
-        likv4.textContent = (+kot + +hlk + +rlk)/sToke + '%';  
+        const ita = document.createElement('p');
+        this.canvas.appendChild(ita);
+        ita.textContent = ((kot/sToke)*100).toFixed(2) + '%';  
         //---------------------------------------    
         
         const l5 = document.createElement('label'); 
         this.canvas.appendChild(l5);
         l5.textContent = 'Hosszú távú adósság:';
         
-        const likv5 = document.createElement('p');
-        this.canvas.appendChild(likv5);
-        likv5.textContent = hlk/(+hlk + +sToke) + '%';  
+        const hta = document.createElement('p');
+        this.canvas.appendChild(hta);
+		var h_st = hlk + sToke;
+        hta.textContent = ((hlk/(hlk + sToke))*100).toFixed(2) + '%';  
         
         //--------------------------------------- 
         
@@ -90,19 +122,19 @@ export class Evaluation extends Application {
         this.canvas.appendChild(l6);
         l6.textContent = 'Működési profithányad:';
         
-        const likv6 = document.createElement('p');
-        this.canvas.appendChild(likv6);
-        likv6.textContent = uzem_eredmeny/ert_net_arb + '%';  
+        const mph = document.createElement('p');
+        this.canvas.appendChild(mph);
+        mph.textContent = ((uzem_eredmeny/ert_net_arb)*100).toFixed(2) + '%';  
         
        //---------------------------------------    
         
         const l7 = document.createElement('label'); 
         this.canvas.appendChild(l7);
-        l7.textContent = 'Nettó profithányad (Árbevétel-arányos adózott eredmény, ROS):';
+        l7.textContent = 'Nettó profithányad (ROS):';
         
-        const likv7 = document.createElement('p');
-        this.canvas.appendChild(likv7);
-        likv7.textContent = aEred/ert_net_arb + '%';  
+        const ros = document.createElement('p');
+        this.canvas.appendChild(ros);
+        ros.textContent = ((aEred/ert_net_arb)*100).toFixed(2) + '%';  
         
         //---------------------------------------    
         
@@ -110,9 +142,9 @@ export class Evaluation extends Application {
         this.canvas.appendChild(l8);
         l8.textContent = 'Eszközarányos adózott eredmény (ROA):';
         
-        const likv8 = document.createElement('p');
-        this.canvas.appendChild(likv8);
-        likv8.textContent = aEred/(+fEszk + +ePapir + +pEszk) + '%';  
+        const roa = document.createElement('p');
+        this.canvas.appendChild(roa);
+        roa.textContent = ((aEred/osszE)*100).toFixed(2) + '%';  
         
         //---------------------------------------    
         
@@ -120,9 +152,9 @@ export class Evaluation extends Application {
         this.canvas.appendChild(l9);
         l9.textContent = 'Sajáttőke-arányos adózott eredmény (ROE):';
         
-        const likv9 = document.createElement('p');
-        this.canvas.appendChild(likv9);
-        likv9.textContent = aEred/sToke + '%';  
+        const roe = document.createElement('p');
+        this.canvas.appendChild(roe);
+        roe.textContent = ((aEred/sToke)*100).toFixed(2) + '%';  
         
         //---------------------------------------    
         
@@ -130,9 +162,9 @@ export class Evaluation extends Application {
         this.canvas.appendChild(l10);
         l10.textContent = 'Árbevétel arányos nyereség:';
         
-        const likv10 = document.createElement('p');
-        this.canvas.appendChild(likv10);
-        likv10.textContent = aEred/ert_net_arb + '%';  
+        const arbev_nyer = document.createElement('p');
+        this.canvas.appendChild(arbev_nyer);
+        arbev_nyer.textContent = ((aEred/ert_net_arb)*100).toFixed(2) + '%';  
         
         //---------------------------------------    
         
@@ -140,19 +172,19 @@ export class Evaluation extends Application {
         this.canvas.appendChild(l11);
         l11.textContent = 'Anyagjellegű ráfordítások aránya:';
         
-        const likv11 = document.createElement('p');
-        this.canvas.appendChild(likv11);
-        likv11.textContent = (+anyag_kolt + +igenybe_szolg + +egyeb_szolg)/(+anyag_kolt + +anyag_raford + +szemely_jel_raford) + '%';  //nem biztos hogy csak ennyi
+        const anyag_rf = document.createElement('p');
+        this.canvas.appendChild(anyag_rf);
+        anyag_rf.textContent = (((anyag_kolt + igenybe_szolg + egyeb_szolg)/ossz_koltseg)*100).toFixed(2) + '%';
         
         //---------------------------------------    
         
         const l12 = document.createElement('label'); 
         this.canvas.appendChild(l12);
-        l12.textContent = 'Hosszú távú adósság:';
+        l12.textContent = 'Személyi jellegű réfordítások:';
         
-        const likv12 = document.createElement('p');
-        this.canvas.appendChild(likv12);
-        likv12.textContent =  szemely_jel_raford/(+anyag_kolt + +anyag_raford + +szemely_jel_raford)+ '%';  
+        const szemelyi_rf = document.createElement('p');
+        this.canvas.appendChild(szemelyi_rf);
+        szemelyi_rf.textContent =  ((szemely_jel_raford/ossz_koltseg)*100).toFixed(2) + '%';  
         
         //---------------------------------------    
             
