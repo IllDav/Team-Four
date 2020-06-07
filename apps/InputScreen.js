cb07eeef3e2cb64818a76ac1cece0f8b4f798242
ricsi828 <deroid0828@gmail.com>
2020-06-07 14:38:45 +0200

    Értékelés gomb

--- apps/InputScreen.js
+++ apps/InputScreen.js
@@ -5,29 +5,30 @@ import Evaluation from './Evaluation.js';
 export class InputScreen extends Application {
     constructor(canvas) {
         super(canvas);
-		
+
         this.initialize();
     }
 
     initialize() {
-		
+
         const evaluate = document.createElement('button');
+        evaluate.className="btn btn-outline-dark";
         this.canvas.appendChild(evaluate);
         evaluate.textContent = 'Értékelés';
         evaluate.id = 'eval';
-		
+
 		let next;
-		
+
 		const evalValues = document.getElementById('eval');
 		evalValues.addEventListener('click', function() {
 		next = new Evaluation(document.getElementById('canvas'));
 		});
-		
+
     }
-	
+
 	update() {
 	}
-	
+
 	cleaner(elementID){
 		document.getElementById(elementID).innerHTML = "";
 	}
